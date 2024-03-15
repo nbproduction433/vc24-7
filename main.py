2bbaf95495cb3354a9f5922183012d1b87ad2f99
nbproduction433 <85150514+nbproduction433@users.noreply.github.com>
2024-03-15 09:51:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def joiner(token, status):
     ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"do not disturb": False}},"s": None,"t": None}
     vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
     ws.send(json.dumps(auth))
     ws.send(json.dumps(vc))
